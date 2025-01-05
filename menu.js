6164f50ccc01cc6cc9ace70c3536ca8465e75f81
Clararyehauge <clry0001@stud.kea.dk>
2025-01-05 20:00:10 +0100

    logo

--- menu.js
+++ menu.js
@@ -12,3 +12,14 @@ function menuClick() {
   burger.classList.remove("active");
   nav.classList.remove("active");
 }
+
+// JavaScript for at håndtere klik og aktiv tilstand
+document.querySelectorAll(".menu-link").forEach((item) => {
+  item.addEventListener("click", function () {
+    // Fjern 'active' klassen fra alle links
+    document.querySelectorAll(".menu-link").forEach((link) => link.classList.remove("active"));
+
+    // Tilføj 'active' klassen til det klikede link
+    item.classList.add("active");
+  });
+});
